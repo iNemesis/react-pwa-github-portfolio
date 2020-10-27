7f0a32310354665aedbf8b9a4c7c10b85b41b2d6
ATMA <12ab@gmx.fr>
2020-10-27 22:00:22 +0100

    :tada: init

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ({ logo }) => (
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+            My Github Portfolio
+        </h1>
+    </header>
+);
+
+export default Header;
---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import './App.css';
+import logo from '../logo.png';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo} />  
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
